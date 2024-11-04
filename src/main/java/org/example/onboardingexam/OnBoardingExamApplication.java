a0ea32c2dcb9eec940c7183636cb41f7c22b9e42
Asem Alfoqha <asem.sh.foqha@gmail.com>
2024-11-04 10:39:18 -0600

    initial commit

---
+++ src/main/java/org/example/onboardingexam/OnBoardingExamApplication.java
@@ -0,0 +1,13 @@
+package org.example.onboardingexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnBoardingExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnBoardingExamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/onboardingexam/OnBoardingExamApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.onboardingexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnBoardingExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
